ed5919ce722a8faff386f274e8a2f0a7cb58b4eb
Brendon Carrasquillo <33206680+brendonwanderlust@users.noreply.github.com>
2019-02-03 17:30:49 -0500

    added Method ovveride

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading needs five or more students."); 
+            }
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            
+            return 'F';
+        }
+
+    }
+}
