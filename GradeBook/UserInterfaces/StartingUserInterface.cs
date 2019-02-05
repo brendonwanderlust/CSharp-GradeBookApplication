1f4db8dfcf613b945f72c9a76c2b894d9451c8c3
Brendon Carrasquillo <33206680+brendonwanderlust@users.noreply.github.com>
2019-02-05 15:56:41 -0500

    Did some stuff

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -33,13 +33,22 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var name = parts[2];
+            if (parts[2] == "standard'")
+            {
+                var gradebook = new StandardGradebook(name);
+            }
+            else if (parts[2] == "ranked")
+            {
+                var gradebook = new RankedGradeBook(name);
+            }
+
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
