e575bb1bfde77d4b1ab124c05a5de5163033f68d
Brendon Carrasquillo <33206680+brendonwanderlust@users.noreply.github.com>
2019-02-05 16:06:57 -0500

    Editied the helpCommand method and createCommand method

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -47,6 +47,10 @@ namespace GradeBook.UserInterfaces
             {
                 var gradebook = new RankedGradeBook(name);
             }
+            else
+            {
+                Console.WriteLine(parts[0] + " is not a supported type of gradebook, please try again");
+            }
 
 
             Console.WriteLine("Created gradebook {0}.", name);
@@ -74,7 +78,7 @@ namespace GradeBook.UserInterfaces
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
